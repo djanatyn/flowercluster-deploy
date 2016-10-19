34bd6e1a4d2e51a1ba9ffa14f1385d12633471a4
Jonathan Strickland <djanatyn@gmail.com>
2016-10-19 12:52:37 -0400

    remove python setup scripts

--- flowercluster-setup/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from oauth2client.client import GoogleCredentials
-from googleapiclient import discovery
-
-credentials = GoogleCredentials.get_application_default()
-compute = discovery.build('compute', 'v1', credentials=credentials)
