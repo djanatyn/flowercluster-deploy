db097fe887ee09d443ef5a44a98cd34372b483d2
Jonathan Strickland <djanatyn@gmail.com>
2016-10-20 09:00:09 -0400

    hide output from running vault unseal, use binary

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@
 import json
 import yaml
 
-from fabric.api import task, run, abort, cd, put
+from fabric.api import task, run, abort, cd, put, prefix, hide
 from fabric_gce_tools import update_roles_gce
 
 config = yaml.load(open('config.yml'))
@@ -14,12 +14,10 @@ config.update(yaml.load(open('secrets/secrets.yml')))
 def unseal_vault():
     """ Unseal the vault using keys specified by the user at deploy time. """
 
-    url = config['api'] + '/sys/unseal'
-
-    for key in config['unseal_keys']:
-        data = json.dumps({'key': key})
-
-        run("curl -X PUT -d '{0}' {1}".format(data, url))
+    with prefix('export VAULT_ADDR=' + config['url']):
+        for key in config['unseal_keys']:
+            with hide('running', 'stdout', 'stderr'):
+                run('vault unseal ' + key)
 
 
 @task
